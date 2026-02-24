e69d7a660f52f14c86eac016ee981dc40869dd1e
bbeck2417 <billys.beck@gmail.com>
2026-02-23 22:49:23 -0500

    modified port usage, remove hardcoded port

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const port = 3100;
+const port = process.env.PORT || 3100;
 const app = express();
 const bodyParser = require("body-parser");
 const Song = require("./models/songs");
